6686f5d08101fae35d6c752c4af7f8fdc7423ac2
Alexsturm1208 <alexanderfalcosturm@gmail.com>
2026-02-19 17:02:19 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Prepared for Electron/Tauri: can switch to 'export' for static builds
-  // output: 'export',
-};
-
-module.exports = nextConfig;
